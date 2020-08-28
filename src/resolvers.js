ca03f968f8dccfe685ebc1c33a1a16f142949c4c
dbrudnicki <dbrudnicki@nmetric.com>
2020-08-28 12:12:01 -0700

    added a little more stuff

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,5 +1,17 @@
+/**
+ * This file resolves the various GraphQL types
+ */
+
+// Array used as a basic database
+const songList = [
+  { title: "Day Tripper", artist: "The Beatles" },
+  { title: "Lucky Man", artist: "Emerson, Lake & Palmer" },
+  { title: "The Beautiful People", artist: "Marilyn Manson" },
+];
+
 module.exports = {
   Query: {
     ping: () => "pong",
+    songs: () => songList,
   },
 };
