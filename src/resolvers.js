3a2574952381c4665958f188fbcc06d2fb6006ea
dbrudnicki <dbrudnicki@nmetric.com>
2020-09-03 17:34:36 -0700

    mutations from talk with Wanye

--- src/resolvers.js
+++ src/resolvers.js
@@ -2,16 +2,52 @@
  * This file resolves the various GraphQL types
  */
 
+let lastId = 3;
+
 // Array used as a basic database
 const songList = [
-  { title: "Day Tripper", artist: "The Beatles" },
-  { title: "Lucky Man", artist: "Emerson, Lake & Palmer" },
-  { title: "The Beautiful People", artist: "Marilyn Manson" },
+  { id: "1", title: "Day Tripper", artist: "The Beatles" },
+  { id: "2", title: "Lucky Man", artist: "Emerson, Lake & Palmer" },
+  { id: "3", title: "The Beautiful People", artist: "Marilyn Manson" },
 ];
 
 module.exports = {
   Query: {
     ping: () => "pong",
-    songs: () => songList,
+    songs: (obj, params) => {
+      if (params.xyz) {
+        if (params.xyz === "ODD") {
+          return songList.filter((s) => Number(s.id) % 2 === 1);
+        } else {
+          return songList.filter((s) => Number(s.id) % 2 === 0);
+        }
+      }
+
+      return songList;
+    },
+  },
+  Mutation: {
+    addSong1: (obj, { title, artist }) => {
+      const song = {
+        id: `${++lastId}`,
+        title,
+        artist,
+      };
+
+      songList.push(song);
+
+      return song;
+    },
+    addSong2: (obj, { args: { title, artist } }) => {
+      const song = {
+        id: `${++lastId}`,
+        title,
+        artist,
+      };
+
+      songList.push(song);
+
+      return song;
+    },
   },
 };
